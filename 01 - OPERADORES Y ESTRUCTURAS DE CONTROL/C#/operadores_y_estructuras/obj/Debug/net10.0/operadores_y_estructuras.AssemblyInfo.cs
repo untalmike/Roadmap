f94c7aa9ac396b371ade676f94d43bdfde1ff2d7
untalmike <miguel.gonzalezl@hotmail.com>
2025-07-02 00:09:03 -0600

    update project, add new folder

--- 01 - OPERADORES Y ESTRUCTURAS DE CONTROL/C#/operadores_y_estructuras/obj/Debug/net10.0/operadores_y_estructuras.AssemblyInfo.cs
+++ 01 - OPERADORES Y ESTRUCTURAS DE CONTROL/C#/operadores_y_estructuras/obj/Debug/net10.0/operadores_y_estructuras.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("operadores_y_estructuras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a3c54a28326e8e369d80a14eff26b3b3278c64a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827173827b3eecd46d81329e314480aa62667d70")]
 [assembly: System.Reflection.AssemblyProductAttribute("operadores_y_estructuras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("operadores_y_estructuras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
